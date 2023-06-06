cd463d30611e9acb0f4bc11cc51588bcf1bf8fc1
afaraitsulaimon <afaraitsulaimon@gmail.com>
2023-06-06 15:41:58 +0100

    fixed the responsiveness of contact us

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -24,8 +24,8 @@ const Contactus = () => {
       </header>
       <main>
         {/* contact us top head start from here */}
-        <section className="w-full h-72 bg-red-500" style={backgroundImage}>
-          <div className="w-full h-full bg-[#8fd0d2] opacity-70 pl-28 pt-48 bg-scroll">
+         <section className="w-[100%] h-72 bg-red-500" style={backgroundImage}>
+          <div className="w-full h-full bg-[#8fd0d2] opacity-70 pl-28 pt-48 ">
             <Link
               to="/"
               className="no-underline text-1xl font-mono hover:text-green-500"
@@ -34,13 +34,13 @@ const Contactus = () => {
             </Link>
             <p className="font-semibold text-4xl">Contact Us</p>
           </div>
-        </section>
+        </section> 
         {/* contact us top head end here */}
 
         {/* contact us content start from here */}
-        <section className='w-full min-h-[100vh] flex justify-center items-center'>
-          <div className="w-[90%] h-[30em] bg-red-400 grid grid-cols-2">
-            <div>
+     <section className='w-full h-[100vh] flex justify-center items-center'>
+          <div className="w-[100%] md:w-[100%] lg:w-[90%] xl:w-[90%] h-[100%] md:h-[100%] lg:h-[30em] bg-red-400 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+            <div className="hidden md:hidden lg:block xl:block">
                 <img src={CustomerservImage2} alt="customer service" className="w-full h-full object-cover" />
             </div>
              
@@ -49,44 +49,44 @@ const Contactus = () => {
                <p className="font-sans text-center pt-2 pb-3 text-white">We're open for Business or just to have a chat</p> 
              
              <div className="w-full flex flex-col justify-between items-center">
-                <div className="w-[85%] h-20 bg-[#6d9b9c] drop-shadow flex items-center rounded-md shadow-lg p-10 m-2">
+                <div className="w-[95%] md:w-[95%] lg:w-[85%] h-20 bg-[#6d9b9c] drop-shadow flex items-center rounded-md shadow-lg p-5 md:p-5 lg:p-10 m-2">
                     <div className="w-10 h-10 flex justify-center items-center border-2 border-white rounded-full">
                         <BiLocationPlus className="text-2xl text-white"/>
                     </div>
-                    <div className="text-white">
-                      <span className="pl-6 font-semibold text-[1rem]">Address: </span><span>198 Brown Road,Lagos, Nigeria,23401</span>
+                    <div className="text-white ">
+                      <span className="pl-3 md:pl-3 lg:pl-6 font-semibold text-[1rem]">Address: </span><span className="">198 Brown Road,Lagos, Nigeria,23401</span>
                     </div>
                 </div>
-                <div className="w-[85%] h-20 bg-[#6d9b9c] flex items-center p-10 m-2 rounded-md ">
+                <div className="w-[95%] md:w-[95%] lg:w-[85%]  h-20 bg-[#6d9b9c] flex items-center p-5 md:p-5 lg:p-10 m-2 rounded-md ">
                     <div className="w-10 h-10 flex justify-center items-center border-2 border-white rounded-full">
                         <FaPhoneAlt className="text-2xl text-white"/>
                     </div>
                     <div className="text-white">
-                      <span className="pl-6 font-semibold text-[1rem]">Phones: </span><span>+234-08043528292</span>
+                      <span className="pl-3 md:pl-3 lg:pl-6 font-semibold text-[1rem]">Phones: </span><span>+234-08043528292</span>
                     </div>
                 </div>
 
-                 <div className="w-[85%] h-20 bg-[#6d9b9c] flex items-center p-10 m-2 rounded-md ">
+                 <div className="w-[95%] md:w-[95%] lg:w-[85%] h-20 bg-[#6d9b9c] flex items-center  p-5 md:p-5 lg:p-10 m-2 rounded-md ">
                     <div className="w-10 h-10 flex justify-center items-center border-2 border-white rounded-full">
                         <HiMail className="text-2xl text-white"/>
                     </div>
                     <div className="text-white">
-                      <span className="pl-6 font-semibold text-[1rem]">Email: </span><span>info@abrydservices.com</span>
+                      <span className="pl-3 md:pl-3 lg:pl-6 font-semibold text-[1rem]">Email: </span><span>info@abrydservices.com</span>
                     </div>
                 </div>
-                <div className="w-[85%] h-20 bg-[#6d9b9c] flex items-center p-10 rounded-md ">
+                <div className="w-[95%] md:w-[95%] lg:w-[85%]  h-20 bg-[#6d9b9c] flex items-center  p-5 md:p-5 lg:p-10 m-2 rounded-md ">
                     <div className="w-10 h-10 flex justify-center items-center border-2 border-white rounded-full">
                         <BiWorld className="text-2xl text-white"/>
                     </div>
                     <div className="text-white">
-                      <span className="pl-6 font-semibold text-[1rem]">Website: </span><span>www.abrydservices.com</span>
+                      <span className="pl-3 md:pl-3 lg:pl-6 font-semibold text-[1rem]">Website: </span><span>www.abrydservices.com</span>
                     </div>
                 </div>
              </div>
              </div>
           </div>
           
-        </section>
+        </section> 
         {/* contact us content end here */}
       </main>
       <footer>
